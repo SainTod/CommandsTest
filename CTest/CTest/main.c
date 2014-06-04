195dfff3ceca879ae1f08f76fa46edcdcb90a49a
Vladimir Dudkin <vo.dudkin@samsung.com>
2014-06-04 19:02:13 +0300

    c project template added

---
+++ CTest/CTest/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  CTest
+//
+//  Created by Vladimir on 6/4/14.
+//  Copyright (c) 2014 Vladimir. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
