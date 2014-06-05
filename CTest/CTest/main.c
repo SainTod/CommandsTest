451c53817b6745635b0cc06288921e9aa5b7b199
Vlad <vlad@Vlad-MacBook-Pro.local>
2014-06-06 00:17:25 +1200

    a

--- CTest/CTest/main.c
+++ CTest/CTest/main.c
@@ -7,39 +7,55 @@
 //
 
 #include <stdio.h>
-bool ProcessingRequest (char* )
+//bool ProcessingRequest (char* )
 
 
-void main(int argc, const char * argv[])
+void main()
 {
-    int n, q, mp[106],i;
+    int n, q, mp[106],i,j;
     char* t, Requests[105];
-    scanf ("%d,%d",N,Q);
+    scanf ("%d,%d",n,q);
     for (i = 0; i<q; i++)
     {
         Requests[i] = (char*) malloc (5*sizeof(char));
         scanf ("%s",t);
         Requests[i] = t;
     }
-    for (i=0;i<n;i++)
+    for (i=0; i<n; i++)
     {
         mp[i] = i;
     }
     for (i=0;i<q;i++)
     {
-        if (Requests[i][1] == U) then
+        if (Requests[i][1] == 'U')
         {
-            if (Requests[i][3] == Requests[i][5]) then
+            if (Requests[i][3] == Requests[i][5])
             {
-                printf(BORE\n);
+                printf("BORE\n");
             }
             else
             {
-                printf(NEWS!\n);
-                
+                printf("NEWS!\n");
+                for (j=0; j<n; j++)
+                {
+                    if (mp[j] == Requests[i][5])
+                    {
+                        mp[j] = Requests[i][3];
+                    }
+                }
+            }
+        }
+        else
+        {
+            if (Requests[i][3] == Requests[i][5])
+            {
+                printf("MUSIC\n");
+            }
+            else
+            {
+                printf("SHOW!\n");
             }
         }
     }
-    
 }
 
